c82c7b00ff271f250af5c186d88f336b9ce811a8
Kelsi H <kelsimhoyle@gmail.com>
2019-07-09 08:59:51 -0600

    Manager can now add more inventory to a product

--- bamazonManager.js
+++ bamazonManager.js
@@ -21,8 +21,8 @@ function menu() {
             "Add New Product",
             "Exit"
         ]
-    }).then(function(answer){
-        switch(answer.action) {
+    }).then(function (answer) {
+        switch (answer.action) {
             case "View Products for Sale":
                 return viewProducts();
             case "View Low Inventory":
@@ -38,21 +38,84 @@ function menu() {
 }
 
 function viewProducts() {
-    connection.query("SELECT * FROM products", function(err, data) {
+    connection.query("SELECT * FROM products", function (err, data) {
         if (err) throw err;
 
+        console.table(data);
+        doNext()
+    });
+}
+
+function viewLow() {
+    connection.query("SELECT * FROM products WHERE stock_quantity < 50", function (err, data) {
+        if (err) throw err;
+        console.log("Low Inventory:")
         console.table(data);
         doNext();
     })
 }
 
-function viewLow() {
-
-}
-
 function addInventory() {
+    connection.query("SELECT * FROM products", function (err, data) {
+        if (err) throw err;
+
+        inquirer.prompt([
+            {
+                message: "What item would you like to update?",
+                type: "list",
+                name: "item",
+                choices: function () {
+                    var choice = data.map(function (product) {
+                        var item = `${product.product_name} $${product.stock_quantity}`;
+                        var id = product.item_id;
+                        return {
+                            value: id,
+                            name: item
+                        };
+                    });
+
+                    return choice;
+                }
+
+            },
+            {
+                message: "How much inventory would you like to add?",
+                name: "quantity",
+                validate: function (input) {
+                    if (isNaN(input)) {
+                        return "Please enter a valid number.";
+                    }
+
+                    return true;
+                }
+            }
+        ]).then(function (answers) {
+            var itemId = answers.item;
+
+            connection.query("SELECT * FROM products WHERE ?", {
+                item_id: itemId
+            }, function (err, data) {
+                var updatedStock = parseInt(answers.quantity) + parseInt(data[0].stock_quantity);
+
+                connection.query("UPDATE products SET ? WHERE ?", [{
+                    stock_quantity: updatedStock
+                }, {
+                    item_id: itemId
+                }], function (err) {
+                    if (err) throw err;
+
+                    console.log(`Added ${answers.quantity} items to ${data[0].product_name}. There are now ${updatedStock} items in stock.\n`);
+
+                    doNext();
+                })
+            })
+
+        })
+    })
 
 }
+
+
 
 function addProduct() {
 
@@ -67,7 +130,7 @@ function doNext() {
             "Go Back to Main Menu",
             "Exit"
         ]
-    }).then(function(answer){
+    }).then(function (answer) {
         if (answer.action === "Go Back to Main Menu") {
             menu();
         } else {
@@ -81,4 +144,3 @@ connection.connect(function (err) {
 
     menu();
 });
-
